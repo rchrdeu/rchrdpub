74dfd892da75488be6169263925c75e40545d2f5
rchrdeu <richard@rchrd.eu>
2023-10-13 11:01:22 +0200

    v0.2 added --force to rm state file

--- foxpost-check.sh
+++ foxpost-check.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# working in actual directory where you download it. I suggest to create a foxpost folder, then this script can work there. 
-# packet list here: 
-csomagszam_list=("CLFOX169677511361839" "CLFOX169685051885433" "CLFOX169687541879937")
+# packet list here:
+csomagszam_list=("CLFOX169718710******" "CLFOX169718710******" "CLFOX169718710******" "CLFOX169718710******")
 
 for csomagszam in "${csomagszam_list[@]}"
 do
@@ -14,24 +13,29 @@ do
     if [[ -f $oldsite ]]; then
             # grep to item date line and export to actual state file
         grep -i -A3 "parcel-status-items__list-item-date" *index.html?code=$csomagszam* > $site
+        sleep 5
             differences=$(diff -u "$oldsite" "$site")
+            sleep 5
         # if no diff, type it in to logs, old state file remove, new to old, send notif to slack webhook
         if [ -z "$differences" ]; then
-            logger foxpost checker: No changes with this package: $csomagszam
-            rm $oldsite
+                sleep 5
+            logger foxpost-checker: No changes with this package: $csomagszam
+            sleep 5
+            rm $oldsite --force
+            sleep 5
             mv $site $oldsite
-             ################## change webhook address!!! #################           
-            curl -X POST -H 'Content-type: application/json' --data '{"text":"No changes with this foxpost package: https://foxpost.hu/csomagkovetes/?code='$csomagszam'"}' PUT YOUR SLACK WEBHOOK ADDRESS HERE 
+            sleep 5
+            # slack webhook notif here: curl -X POST -H 'Content-type: application/json' --data '{"text":"No changes with this foxpost package: https://foxpost.hu/csomagkovetes/?code='$csomagszam'"}' *** webhook here ***
         else
             # else, send update if there are differences
-            logger foxpost checker: New updates, sent via slack webhook. $csomagszam
-            ################## change webhook address!!! #################
-            curl -X POST -H 'Content-type: application/json' --data '{"text":"New changes with the package: https://foxpost.hu/csomagkovetes/?code='$csomagszam'"}' PUT YOUR SLACK WEBHOOK ADDRESS HERE
+            logger foxpost-checker: New updates, sent via slack webhook. $csomagszam
+            sleep 5
+            # slack webhook! curl -X POST -H 'Content-type: application/json' --data '{"text":"New changes with the package: https://foxpost.hu/csomagkovetes/?code='$csomagszam'"}' ***webhook here***
         fi
 
     else
         grep -i -A3 "parcel-status-items__list-item-date" *index.html?code=$csomagszam* > $oldsite
     fi
-
-    rm *index.html?code=$csomagszam*
+        sleep 5
+    rm *index.html?code=$csomagszam* --force
 done
